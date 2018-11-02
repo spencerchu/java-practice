a1270dc13fea08eba2b519a7c03eb97baa6b17e3
Spencer C <spencerbchu@gmail.com>
2018-11-01 20:47:05 -0400

    Setup

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Standard 'Hello World' first program
+ * 
+ * @author Everyone who has ever written about programming and Tim Pierson too, 
+ * Dartmouth CS 10, Fall 2018
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
